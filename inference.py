140081f625c03d75f592a9e253e005454e8b70e1
william-zhang-ml <william.zhang.ml@gmail.com>
2023-08-18 08:25:01 -0400

    add function to test onnx model produces same output as original

--- inference.py
+++ inference.py
@@ -1,7 +1,44 @@
 """ Useful utilities for inferencing but not needed for training. """
+import tempfile
 from typing import List, Tuple
 import torch
+from torch import nn
 import onnxruntime as ort
+
+
+def assert_siso_onnx_output_same(
+    model: nn.Module,
+    example_inp: torch.Tensor,
+    num_trials: int = 10
+) -> None:
+    """Test whether a model will produce same output if exported to ONNX.
+
+    Args:
+        model (nn.Module): model of interest in eval mode
+        example_inp (torch.Tensor): example of valid model input
+        num_trials (int): number of times to compare outputs
+    """
+    assert not model.training, 'must pass model in eval mode'
+
+    # create onnx inference session by exporting to temp file
+    with tempfile.TemporaryFile() as file:
+        torch.onnx.export(
+            model,
+            example_inp,
+            file,
+            input_names=['input'],
+            output_names=['output'],
+        )
+        file.seek(0)
+        sess = ort.InferenceSession(file.read())
+
+    # run random tensors through both models
+    for _ in range(num_trials):
+        inp = torch.rand_like(example_inp)
+        with torch.no_grad():
+            model_outp = model(inp)
+        onnx_outp = sess.run(None, {'input': inp.numpy()})[0]
+        torch.testing.assert_allclose(model_outp, onnx_outp)
 
 
 class OnnxEngine:
